ab0166109c3b748239b2eb1b20476de949811f3b
Anuraj1709 <anurajanand2002@gmail.com>
2026-08-02 13:18:59 +0530

    Use deployed backend URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,17 +1,14 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://127.0.0.1:8000"
+    baseURL: import.meta.env.VITE_API_URL
 });
 
 export const sendMessage = async (sessionId, message) => {
 
     const response = await API.post("/chat", {
-
         session_id: sessionId,
-
         message: message
-
     });
 
     return response.data;
